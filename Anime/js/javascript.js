d09c71f69de596fe5e87e9e263df92ea24a78e9c
David Beamonde <117319667+pokemaniaco-161@users.noreply.github.com>
2023-02-26 01:01:10 +0100

    Pokemon y Anime

---
+++ Anime/js/javascript.js
@@ -0,0 +1,13 @@
+function abrirMenu(){
+    document.getElementById('menu1').innerHTML='<div class="pags"><a href="../index.html">Inicio</a></div>';
+    document.getElementById('cabecera').innerHTML='<div class="m1"><h1>Ver anime</h1></div>';
+}
+function abrirMenu2(){
+    document.getElementById('menu').innerHTML='<div class="pags"><a href="index.html">Inicio</a></div>';
+    document.getElementById('cabecera').innerHTML='<div class="m1"><h1>Ver anime y hentai</h1></div>';
+}
+
+function pieDePagina(){
+    document.getElementById('piepagina').innerHTML='- @ David Beamonde Salinas'
+}
+
