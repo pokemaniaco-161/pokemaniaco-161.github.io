5f673dba6b50c054e15c1b9b8ae230fea7022821
David Beamonde <117319667+pokemaniaco-161@users.noreply.github.com>
2023-02-26 23:05:43 +0100

    Pokemon y Anime

--- Anime/js/javascript.js
+++ Anime/js/javascript.js
@@ -1,18 +1,18 @@
 function abrirMenu(){
-    document.getElementById('menu1').innerHTML='<div class="pags"><a href="../index.html">Inicio</a></div>';
+    document.getElementById('menu1').innerHTML='<div class="pags"><a href="../../index.html">Inicio</a></div> <div class="pags"><a href="../index.html">Anime y Hentai</a></div>';
     document.getElementById('cabecera').innerHTML='<div class="m1"><h1>Ver Anime</h1></div>';
 }
 function abrirMenu2(){
-    document.getElementById('menu').innerHTML='<div class="pags"><a href="index.html">Inicio</a></div>';
+    document.getElementById('menu').innerHTML='<div class="pags"><a href="../index.html">Inicio</a></div> <div class="pags"><a href="index.html">Anime y Hentai</a></div>';
     document.getElementById('cabecera').innerHTML='<div class="m1"><h1>Ver Anime y Hentai</h1></div>';
 }
 function abrirMenuH(){
-    document.getElementById('menu1').innerHTML='<div class="pags"><a href="../index.html">Inicio</a></div>';
+    document.getElementById('menu1').innerHTML='<div class="pags"><a href="../../index.html">Inicio</a></div> <div class="pags"><a href="../index.html">Anime y Hentai</a></div>';
     document.getElementById('cabecera').innerHTML='<div class="m1"><h1>Ver Hentai</h1></div>';
 }
 
 
 function pieDePagina(){
-    document.getElementById('piepagina').innerHTML='- @ David Beamonde Salinas'
+    document.getElementById('piepagina').innerHTML='- @ Pokemaniaco-161'
 }
 
